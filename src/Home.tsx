8e03b84689f385c4d528b29a4f6c7ed0512b0e72
Henrique Keiti <henrique.keiti2@gmail.com>
2020-02-17 18:15:57 -0300

    teste
    2

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,14 +4,14 @@ import './Home.css';
 const Home: React.FC = () => (
 <div className="container" >
   <br></br>
-  <h2><p className="text-center">Exemplo PWA com react</p></h2>
+  <h2><p className="text-center">Exemplo PWA com React</p></h2>
   <div className="text-center">
   <img src="https://miro.medium.com/max/700/1*owd84a_YqEeNjP9frRyzIQ.png" className="img-fluid" alt="Imagem responsiva"></img>
   </div>
   <div className="card" >
   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTORlgCZdiBGan-MDdnQSmg1f5HUUi2DSYiuf-NEu_KnmDX8-M-" className="card-img-top" alt="..."></img>
   <div className="card-body">
-    <h5 className="card-title">Lighthouse</h5>
+    <h5 className="card-title">+Lighthouse</h5>
     <p className="card-text">Ferramenta automatizada de código aberto que aprimora a qualidade de apps da Web.</p>
   </div>
   <div className="card-body">
